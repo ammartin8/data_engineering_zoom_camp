d8915c62db51c34f135bb26ccbbcf1d7b7421cc9
AM <ammartin88@protonmail.com>
2026-01-14 18:12:57 -0500

    Reorganize file project structure, create terraform module files

---
+++ modules/module_1/terraform_workshop/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.16.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = ""
+  project     = ""
+  region      = "us-central1"
+}
+
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = ""
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
